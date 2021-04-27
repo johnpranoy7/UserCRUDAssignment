b6442a2146c4a208e8a929b8dcc48249c63bfd6e
jyalla <jyalla@JYALLA-L-2081.evoketechnologies.com>
2021-04-27 19:20:53 +0530

    Modified Exceptions

--- src/main/java/com/jyalla/demo/exception/RestExceptionHandler.java
+++ src/main/java/com/jyalla/demo/exception/RestExceptionHandler.java
@@ -1,23 +1,17 @@
 package com.jyalla.demo.exception;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice
+// @Order(Ordered.HIGHEST_PRECEDENCE)
+// @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        // TODO Auto-generated method stub
-        return ResponseEntity.ok(new ErrorDTO(status.value(), ex.getLocalizedMessage()));
-    }
+    // @Override
+    // protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+    // HttpHeaders headers, HttpStatus status, WebRequest request) {
+    // // TODO Auto-generated method stub
+    // return ResponseEntity.ok(new ErrorDTO(status.value(), ex.getLocalizedMessage()));
+    // }
 
     private String getCustomMessage(int status) {
         switch (status) {
