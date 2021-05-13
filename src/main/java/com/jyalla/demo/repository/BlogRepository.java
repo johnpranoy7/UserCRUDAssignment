604fc62627a2d95080f2bf4704151e9a673681aa
jyalla <jyalla@JYALLA-L-2081.evoketechnologies.com>
2021-05-13 20:57:10 +0530

    Integrated Flyway

--- src/main/java/com/jyalla/demo/repository/BlogRepository.java
+++ src/main/java/com/jyalla/demo/repository/BlogRepository.java
@@ -3,12 +3,11 @@ package com.jyalla.demo.repository;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import com.jyalla.demo.modal.Blog;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "blogs", path = "blogs")
+// @RepositoryRestResource(collectionResourceRel = "blogs", path = "blogs")
 public interface BlogRepository extends JpaRepository<Blog, UUID> {
 
     List<Blog> findByTitle(String title);
